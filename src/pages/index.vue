0acce2bb915e2cd6c0f57fc9603970f305117df7
Jc Cloete <jcccloete97@gmail.com>
2022-01-14 16:42:40 +0200

    Clean up file

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,68 +1,81 @@
 <script setup lang="ts">
-  const secretWord = "tangy";
-  const inputWord = ref('');
+import { Ref } from 'vue';
 
-  const gameGridData: any = ref([])
-  const gameCount = ref(0);
+declare type GameCell = {
+  color: string,
+  char: string
+}
 
-  function validateWordGuess(){
-    let result = [];
-    for(let i = 0; i < 5; i++){
-      const guessChar = inputWord.value.charAt(i).toLowerCase();
-      const secretChar = secretWord.charAt(i).toLowerCase();
-      if(guessChar === secretChar){
-        result.push({color: 'green', char: guessChar});
-      }else if(secretWord.includes(guessChar)) { 
-        result.push({color: 'yellow', char: guessChar});
-      }else{
-        result.push({color: 'gray', char: guessChar});
+const secretWord: string = "tangy";
+const inputWord: Ref<string> = ref('');
+
+const gameGridData: Ref<GameCell[][]> = ref([[]]);
+const gameCount: Ref<number> = ref(0);
+
+//Loaded words from wordlist.txt
+let words: string[] = [];
+
+//Load the wordlist text file onMount (lifecycle hook)
+onMounted(()=>{
+  fetch(`/wordlist.txt`).then(async result =>{
+    const data = (await result.text()).split('\r\n');
+    const fiveChar = data.filter(string =>{
+      const length = string.length;
+      if(length == 5){
+        return true;
       }
+      return false;
+    });
+
+    words = fiveChar;
+  });
+});
+
+function handleButtonClick(){
+  //1. Check for valid word, if not valid stop early.
+  const validWord = isValidWord(inputWord.value)
+  if(!validWord){
+    window.alert("Word is invalid, please try again!")
+    inputWord.value = "";
+    return;
+  }
+
+  //2.Validate the guess and populate gridData for the round
+  const data = gameGridData.value;
+  data[gameCount.value] = validateWordGuess();
+  gameGridData.value = data;
+
+  //3. Increase counter and reset input value
+  gameCount.value += 1;
+  inputWord.value = "";
+
+  //4. You only have 6 tries, after 6 tries the game will restart (same word)
+  if(gameCount.value === 6){
+    window.alert("Game over, please try again");
+    gameGridData.value = [];
+    gameCount.value = 0;
+  }
+}
+
+function validateWordGuess(): GameCell[]{
+  let result = [];
+  for(let i = 0; i < 5; i++){
+    const guessChar = inputWord.value.charAt(i).toLowerCase();
+    const secretChar = secretWord.charAt(i).toLowerCase();
+    if(guessChar === secretChar){
+      result.push({color: 'green', char: guessChar});
+    }else if(secretWord.includes(guessChar)) { 
+      result.push({color: 'yellow', char: guessChar});
+    }else{
+      result.push({color: 'gray', char: guessChar});
     }
-    return result;
   }
+  return result;
+}
 
-  function isValidWord(word: string){
-    return new Promise((resolve, reject) =>{
-      fetch(`/wordlist.txt`).then(async result =>{
-        const data = (await result.text()).split('\r\n');
-        const fiveChar = data.filter(string =>{
-          const length = string.length;
-          if(length == 5){
-            return true;
-          }
-          return false;
-        });
-
-        if(fiveChar.includes(word.toLowerCase())){
-          resolve(true);
-        }else{
-          resolve(false);
-        }
-      });
-    });
-  }
-
-  function handleButtonClick(){
-    //call validation
-    isValidWord(inputWord.value).then(result =>{
-      
-      if(!result){
-        window.alert("Word is invalid, please try again!")
-        inputWord.value = "";
-        return;
-      }
-
-      const data = gameGridData.value as any;
-      data[gameCount.value] = validateWordGuess();
-      gameCount.value += 1;
-      inputWord.value = "";
-      if(gameCount.value === 6){
-        window.alert("Game over, please try again");
-        gameGridData.value = [];
-        gameCount.value = 0;
-      }
-    });
-  }
+function isValidWord(word: string){
+  return words.includes(word.toLowerCase());
+}
 </script>
 
 <template>
@@ -84,11 +97,9 @@
     </div>
   </div>
 </div>
-
 </template>
 
 <style scoped>
-
 .game-container{
   @apply container mr-auto ml-auto w-96 p-2;
 }
